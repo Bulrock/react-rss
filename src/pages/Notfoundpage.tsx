03996740526d048f8960ff2c57e5b15be57b4d0e
Bulrock <maksim.volozhinskij@gmail.com>
2023-03-13 19:19:18 +0100

    feat: implement unit tests for NotFoundPage

--- src/pages/Notfoundpage.tsx
+++ src/pages/Notfoundpage.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Header from 'components/Header';
+import Header from '../../src/components/Header';
 
 class NotFoundPage extends Component {
   render() {
     return (
       <div>
         <Header hideSearch={true} />
-        <h1>404</h1>
-        <p>
+        <h1 data-testid="not-found-h1">404</h1>
+        <p data-testid="not-found-p">
           This page does not exist. Go{' '}
-          <Link className="not-found-link" to="/">
+          <Link className="not-found-link" to="/" data-testid="not-found-link">
             home
           </Link>
         </p>

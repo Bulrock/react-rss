0bef60e4099564dec5dc19a9349d4f650501b7d0
Bulrock <maksim.volozhinskij@gmail.com>
2023-03-15 17:41:26 +0100

    refactor: update favicon and Not Found Page style

--- src/pages/Notfoundpage.tsx
+++ src/pages/Notfoundpage.tsx
@@ -2,19 +2,23 @@ import * as React from 'react';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../src/components/Header';
+import notFound from '../assets/notFound.png';
 
 class NotFoundPage extends Component {
   render() {
     return (
       <div>
         <Header hideSearch={true} />
-        <h1 data-testid="not-found-h1">404</h1>
-        <p data-testid="not-found-p">
-          This page does not exist. Go{' '}
+        <h1 className="not-found-title" data-testid="not-found-h1">
+          404
+        </h1>
+        <p className="not-found-p" data-testid="not-found-p">
+          This Universe does not exist. Go your home{' '}
           <Link className="not-found-link" to="/" data-testid="not-found-link">
-            home
+            planet
           </Link>
         </p>
+        <img className="not-found-img" src={notFound} />
       </div>
     );
   }

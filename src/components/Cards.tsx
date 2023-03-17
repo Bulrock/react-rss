7bb49ab25c02932c8157c5118b5e255ff3842406
Bulrock <maksim.volozhinskij@gmail.com>
2023-03-17 15:31:00 +0100

    feat: implement unit tests for Cards component

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -5,7 +5,7 @@ import { IPerson } from 'models/types';
 class Cards extends Component<{ persons: IPerson[] }> {
   render() {
     return (
-      <div className="cards">
+      <div className="cards" data-testid="cards">
         {this.props.persons.map((person) => {
           return <Card person={person} key={String(person.id)} />;
         })}

9664a1be5e9444d30eccadf0ec7a38d8ee6d8d23
Bulrock <maksim.volozhinskij@gmail.com>
2023-03-17 16:34:20 +0100

    refactor: pages styles and About page text

--- src/pages/Aboutpage.tsx
+++ src/pages/Aboutpage.tsx
@@ -9,8 +9,12 @@ class AboutPage extends Component {
       <>
         <Header hideSearch={true} />
         <div className="main">
-          <h1 data-testid="about-h1">About us</h1>
-          <p data-testid="about-p">This is a RSS React task website</p>
+          <h1 data-testid="about-h1">What is this?</h1>
+          <p data-testid="about-p">
+            Website based on the television show Rick and Morty. You will have access to about
+            hundreds of characters. It filled with information as seen on the TV show from The Rick
+            and Morty API.
+          </p>
         </div>
         <Footer />
       </>

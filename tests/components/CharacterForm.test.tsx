a3063456db01048cab77bd70c7cb38810956df3a
Bulrock <maksim.volozhinskij@gmail.com>
2023-04-15 23:10:19 +0200

    refactor: CharacterForm component unit tests

--- tests/components/CharacterForm.test.tsx
+++ tests/components/CharacterForm.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { unmountComponentAtNode } from 'react-dom';
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
@@ -29,37 +29,37 @@ afterEach(() => {
 
 describe('CharacterForm', () => {
   it('renders correctly', async () => {
-    render(
+    const { getByLabelText, getByTestId, getByRole } = render(
       <Provider store={store}>
         <CharacterForm />
       </Provider>
     );
     waitFor(() => {
-      expect(screen.getByLabelText('Name:')).toBeInTheDocument();
-      expect(screen.getByTestId('status-0')).toBeInTheDocument();
-      expect(screen.getByLabelText('Species:')).toBeInTheDocument();
-      expect(screen.getByLabelText('Gender:')).toBeInTheDocument();
-      expect(screen.getByLabelText('Origin planet of birth:')).toBeInTheDocument();
-      expect(screen.getByLabelText('Last known location:')).toBeInTheDocument();
-      expect(screen.getByLabelText('Image:')).toBeInTheDocument();
-      expect(screen.getByLabelText('Date of character creation:')).toBeInTheDocument();
-      expect(screen.getByLabelText('I consent to this data')).toBeInTheDocument();
-      expect(screen.getByRole('button')).toBeInTheDocument();
+      expect(getByLabelText('Name:')).toBeInTheDocument();
+      expect(getByTestId('status-0')).toBeInTheDocument();
+      expect(getByLabelText('Species:')).toBeInTheDocument();
+      expect(getByLabelText('Gender:')).toBeInTheDocument();
+      expect(getByLabelText('Origin planet of birth:')).toBeInTheDocument();
+      expect(getByLabelText('Last known location:')).toBeInTheDocument();
+      expect(getByLabelText('Image:')).toBeInTheDocument();
+      expect(getByLabelText('Date of character creation:')).toBeInTheDocument();
+      expect(getByLabelText('I consent to this data')).toBeInTheDocument();
+      expect(getByRole('button')).toBeInTheDocument();
     });
   });
 
   it('should not return Icharacter on submit with incorrectly filled form', async () => {
     let submitedICharacter: ICharacter;
 
-    render(
+    const { getByTestId } = render(
       <Provider store={store}>
         <CharacterForm />
       </Provider>
     );
 
-    userEvent.type(screen.getByTestId('name'), 'Morty');
+    userEvent.type(getByTestId('name'), 'Morty');
 
-    const submitButton = screen.getByTestId('form-submit-btn');
+    const submitButton = getByTestId('form-submit-btn');
 
     fireEvent.click(submitButton);
 
@@ -71,29 +71,29 @@ describe('CharacterForm', () => {
   it('should return Icharacter on submit with correctly filled form', async () => {
     let submitedICharacter: ICharacter;
 
-    render(
+    const { getByLabelText, getByTestId } = render(
       <Provider store={store}>
         <CharacterForm />
       </Provider>
     );
 
-    fireEvent.change(screen.getByLabelText('Name:'), { target: { value: 'Rick Sanchez' } });
-    fireEvent.click(screen.getByTestId('status-0'));
-    fireEvent.change(screen.getByLabelText('Species:'), { target: { value: 'Human' } });
-    fireEvent.change(screen.getByLabelText('Gender:'), { target: { value: 'Male' } });
-    fireEvent.change(screen.getByLabelText('Origin planet of birth:'), {
+    fireEvent.change(getByLabelText('Name:'), { target: { value: 'Rick Sanchez' } });
+    fireEvent.click(getByTestId('status-0'));
+    fireEvent.change(getByLabelText('Species:'), { target: { value: 'Human' } });
+    fireEvent.change(getByLabelText('Gender:'), { target: { value: 'Male' } });
+    fireEvent.change(getByLabelText('Origin planet of birth:'), {
       target: { value: 'Earth' },
     });
-    fireEvent.change(screen.getByLabelText('Last known location:'), { target: { value: 'Earth' } });
-    fireEvent.change(screen.getByLabelText('Image:'), {
+    fireEvent.change(getByLabelText('Last known location:'), { target: { value: 'Earth' } });
+    fireEvent.change(getByLabelText('Image:'), {
       target: { files: [new File(['test'], 'test.png', { type: 'image/png' })] },
     });
-    fireEvent.change(screen.getByLabelText('Date of character creation:'), {
+    fireEvent.change(getByLabelText('Date of character creation:'), {
       target: { value: '2022-01-01' },
     });
-    fireEvent.click(screen.getByLabelText('I consent to this data'));
+    fireEvent.click(getByLabelText('I consent to this data'));
 
-    fireEvent.click(screen.getByTestId('form-submit-btn'));
+    fireEvent.click(getByTestId('form-submit-btn'));
 
     waitFor(() => {
       expect(submitedICharacter).toBeDefined();
@@ -101,76 +101,74 @@ describe('CharacterForm', () => {
   });
 
   it('should return errors on submit with empty form fields', async () => {
-    render(
+    const { getByText, getByTestId } = render(
       <Provider store={store}>
         <CharacterForm />
       </Provider>
     );
 
-    fireEvent.click(screen.getByTestId('form-submit-btn'));
+    fireEvent.click(getByTestId('form-submit-btn'));
 
     await waitFor(() => {
-      expect(screen.getByText('Name is required')).toBeInTheDocument();
-      expect(screen.getByText('Status is required')).toBeInTheDocument();
-      expect(screen.getByText('Species is required')).toBeInTheDocument();
-      expect(screen.getByText('Gender is required')).toBeInTheDocument();
-      expect(screen.getByText('Origin planet name is required')).toBeInTheDocument();
-      expect(screen.getByText('Last known location is required')).toBeInTheDocument();
-      expect(screen.getByText('Select character image')).toBeInTheDocument();
-      expect(screen.getByText('Select date of creation')).toBeInTheDocument();
-      expect(
-        screen.getByText('Confirm information publishing before submitting')
-      ).toBeInTheDocument();
+      expect(getByText('Name is required')).toBeInTheDocument();
+      expect(getByText('Status is required')).toBeInTheDocument();
+      expect(getByText('Species is required')).toBeInTheDocument();
+      expect(getByText('Gender is required')).toBeInTheDocument();
+      expect(getByText('Origin planet name is required')).toBeInTheDocument();
+      expect(getByText('Last known location is required')).toBeInTheDocument();
+      expect(getByText('Select character image')).toBeInTheDocument();
+      expect(getByText('Select date of creation')).toBeInTheDocument();
+      expect(getByText('Confirm information publishing before submitting')).toBeInTheDocument();
     });
   });
 
   it('submits the form correctly with message and reset the form', async () => {
-    render(
+    const { getByLabelText, getByTestId, findByTestId, getByRole } = render(
       <Provider store={store}>
         <CharacterForm />
       </Provider>
     );
-    const submitButton = screen.getByRole('button');
+    const submitButton = getByRole('button');
 
-    fireEvent.change(screen.getByLabelText('Name:'), { target: { value: 'Rick Sanchez' } });
-    fireEvent.click(screen.getByTestId('status-0'));
-    fireEvent.change(screen.getByLabelText('Species:'), { target: { value: 'Human' } });
-    fireEvent.change(screen.getByLabelText('Gender:'), { target: { value: 'Male' } });
-    fireEvent.change(screen.getByLabelText('Origin planet of birth:'), {
+    fireEvent.change(getByLabelText('Name:'), { target: { value: 'Rick Sanchez' } });
+    fireEvent.click(getByTestId('status-0'));
+    fireEvent.change(getByLabelText('Species:'), { target: { value: 'Human' } });
+    fireEvent.change(getByLabelText('Gender:'), { target: { value: 'Male' } });
+    fireEvent.change(getByLabelText('Origin planet of birth:'), {
       target: { value: 'Earth' },
     });
-    fireEvent.change(screen.getByLabelText('Last known location:'), { target: { value: 'Earth' } });
-    fireEvent.change(screen.getByLabelText('Image:'), {
+    fireEvent.change(getByLabelText('Last known location:'), { target: { value: 'Earth' } });
+    fireEvent.change(getByLabelText('Image:'), {
       target: { files: [new File(['test'], 'test.png', { type: 'image/png' })] },
     });
-    fireEvent.change(screen.getByLabelText('Date of character creation:'), {
+    fireEvent.change(getByLabelText('Date of character creation:'), {
       target: { value: '2022-01-01' },
     });
-    fireEvent.click(screen.getByLabelText('I consent to this data'));
+    fireEvent.click(getByLabelText('I consent to this data'));
 
     fireEvent.submit(submitButton);
-    const submitMessage = await screen.findByTestId('submit-message');
-    waitFor(() => {
-      expect(submitMessage).toHaveClass('submit-message');
+    const submitMessage = await findByTestId('submit-message');
+    await waitFor(() => {
+      expect(submitMessage).not.toHaveClass('submit-message');
     });
 
-    waitFor(() => {
+    await waitFor(() => {
       expect(submitMessage).toHaveTextContent(/Data has been saved/i);
       expect(submitMessage).toHaveClass('notsubmit-message');
     });
 
     waitFor(() => {
       expect(submitMessage).not.toHaveTextContent(/Data has been saved/i);
-      expect(submitMessage).toHaveClass('submit-message');
-      expect(screen.getByLabelText('Name:')).toHaveValue('Rick Sanchez');
-      expect(screen.getByTestId('status-0')).not.toBeChecked();
-      expect(screen.getByLabelText('Species:')).toHaveValue('');
-      expect(screen.getByLabelText('Gender:')).toHaveValue('');
-      expect(screen.getByLabelText('Origin planet of birth:')).toHaveValue('');
-      expect(screen.getByLabelText('Last known location:')).toHaveValue('');
-      expect(screen.getByLabelText('Image:') as HTMLInputElement).toHaveValue('');
-      expect(screen.getByLabelText('Date of character creation:')).toHaveValue('');
-      expect(screen.getByLabelText('I consent to this data')).not.toBeChecked();
+      expect(submitMessage).not.toHaveClass('submit-message');
+      expect(getByLabelText('Name:')).toHaveValue('Rick Sanchez');
+      expect(getByTestId('status-0')).not.toBeChecked();
+      expect(getByLabelText('Species:')).toHaveValue('');
+      expect(getByLabelText('Gender:')).toHaveValue('');
+      expect(getByLabelText('Origin planet of birth:')).toHaveValue('');
+      expect(getByLabelText('Last known location:')).toHaveValue('');
+      expect(getByLabelText('Image:') as HTMLInputElement).toHaveValue('');
+      expect(getByLabelText('Date of character creation:')).toHaveValue('');
+      expect(getByLabelText('I consent to this data')).not.toBeChecked();
     });
   });
 });

053d8b7ec723bfb9dc9a56e927b2eee20e95ea99
Bulrock <maksim.volozhinskij@gmail.com>
2023-04-18 22:07:10 +0200

    refactor: eslint configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), eslint(), viteTsconfigPaths(), svgrPlugin()],
 });
